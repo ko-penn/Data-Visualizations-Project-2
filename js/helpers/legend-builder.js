188f403412028fc6fba701209d4e1cd7f56cd6c1
Devin Harris <devinharris.316@gmail.com>
2024-03-18 21:04:08 -0400

    legend updates

--- js/helpers/legend-builder.js
+++ js/helpers/legend-builder.js
@@ -1,76 +1,81 @@
 export class LegendBuilder {
-  legend = null;
+   legend = null;
 
-  constructor(parentElement) {
-    this.legend = parentElement.querySelector("legend");
-    if (!this.legend) {
-      this.legend = document.createElement("legend");
-      parentElement.append(this.legend);
-    }
+   constructor(parentElement, _toggleCallback) {
+      this.legend = parentElement.querySelector('legend');
+      this.toggleCallback = _toggleCallback;
+      if (!this.legend) {
+         this.legend = document.createElement('div');
+         this.legend.classList.add('legend');
+         parentElement.append(this.legend);
+      }
 
-    this.selectedLegendGroups = new Set();
-    if (!this.legend) {
-      console.error("Legend element does not exist");
-    }
-    if (this.legend.children.length === 0) {
-      this.buildLegend();
-    }
-  }
+      this.selectedLegendGroups = new Set();
+      if (!this.legend) {
+         console.error('Legend element does not exist');
+      }
+      if (this.legend.children.length === 0) {
+         this.buildLegend();
+      }
+   }
 
-  buildLegend() {
-    const legendElm = document.createElement("legend");
-    legendElm.innerText = "Legend";
+   buildLegend() {
+      const legendElm = document.createElement('legend');
+      legendElm.innerText = 'Legend';
 
-    this.fieldSetElm = document.createElement("fieldset");
-    this.fieldSetElm.append(legendElm);
-    this.fieldSetElm.classList.add("legend-content");
+      this.fieldSetElm = document.createElement('fieldset');
+      this.fieldSetElm.append(legendElm);
+      this.fieldSetElm.classList.add('legend-content');
 
-    this.legend.append(this.fieldSetElm);
-  }
+      this.legend.append(this.fieldSetElm);
+   }
 
-  setLegendColorScale(colorScale) {
-    if (
-      JSON.stringify(colorScale.domain()) === JSON.stringify(this.storedDomain)
-    ) {
-      return;
-    }
+   setLegendColorScale(colorScale) {
+      if (
+         JSON.stringify(colorScale.domain()) ===
+         JSON.stringify(this.storedDomain)
+      ) {
+         return;
+      }
 
-    this.storedDomain = colorScale.domain();
+      this.storedDomain = colorScale.domain();
 
-    this.removeFieldSet();
+      this.removeFieldSet();
 
-    const legendColors = document.createElement("div");
-    legendColors.classList.add("legend-colors");
+      const legendColors = document.createElement('div');
+      legendColors.classList.add('legend-colors');
 
-    colorScale.domain().forEach((d) => {
-      const p = document.createElement("p");
-      const span = document.createElement("span");
-      span.style.background = colorScale(d);
-      span.addEventListener("click", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        if (this.selectedLegendGroups.has(d)) {
-          this.selectedLegendGroups.delete(d);
-          p.classList.remove("selected");
-        } else {
-          this.selectedLegendGroups.add(d);
-          p.classList.add("selected");
-        }
+      colorScale.domain().forEach((d) => {
+         const p = document.createElement('p');
+         const span = document.createElement('span');
+         span.style.background = colorScale(d);
+         span.addEventListener('click', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            if (this.selectedLegendGroups.has(d)) {
+               this.selectedLegendGroups.delete(d);
+               p.classList.remove('selected');
+            } else {
+               this.selectedLegendGroups.add(d);
+               p.classList.add('selected');
+            }
+
+            this.toggleCallback();
+         });
+         p.append(span);
+         p.append(d);
+         p.classList.add('selected');
+         this.selectedLegendGroups.add(d);
+         legendColors.append(p);
       });
-      p.append(span);
-      p.append(d);
-      p.classList.add("selected");
-      this.selectedLegendGroups.add(d);
-      legendColors.append(p);
-    });
 
-    this.fieldSetElm.append(legendColors);
-  }
+      this.fieldSetElm.append(legendColors);
+   }
 
-  removeFieldSet() {
-    Array.from(this.fieldSetElm.children).forEach((element, i) => {
-      if (i !== 0) element.remove();
-    });
-    this.selectedLegendGroups.clear();
-  }
+   removeFieldSet() {
+      Array.from(this.fieldSetElm.children).forEach((element, i) => {
+         if (i !== 0) element.remove();
+      });
+      this.selectedLegendGroups.clear();
+   }
 }
